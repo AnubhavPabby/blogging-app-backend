76fc9e5303467b11095c40411cd2fd4023c525f4
Anubhav Pabby <anubhavpabby@gmail.com>
2023-01-23 15:01:26 +0530

    Initialized and setuped spring-boot application.

---
+++ src/test/java/com/anubhavpabby/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anubhavpabby.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
