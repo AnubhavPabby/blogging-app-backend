39c5a68188c8056009721779dfc65b56f952b932
Anubhav Pabby <anubhavpabby@gmail.com>
2023-01-23 15:36:57 +0530

    Configured swagger api using spring-doc-open-api dependency

--- src/main/java/com/anubhavpabby/blog/BlogAppApisApplication.java
+++ src/main/java/com/anubhavpabby/blog/BlogAppApisApplication.java
@@ -2,8 +2,11 @@ package com.anubhavpabby.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BlogAppApisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
